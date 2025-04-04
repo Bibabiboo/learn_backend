0a3fef5a33ee6a0d822eddfffea03adcef0732d5
Bibabiboo <phuthinh8947@gmail.com>
2025-04-04 12:31:13 +0700

    Init Code

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const hostname = 'localhost'
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port http://${hostname}${port}`)
+})
